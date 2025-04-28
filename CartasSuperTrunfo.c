7191019e63c77b7f3ead3b6bf8c10867270a6ec6
BrunoEV12 <brunodev5405@gmail.com>
2025-04-28 23:15:05 +0000

    Versão 3.0 Modificado tipo de Variavel da Area de (int) para (float). Adicionado calculo de Densidade Populacional e PIB Per Capita, para CARTAS 1 e 2. Adicionado Densidade Populacional e PIB Per Capita na apresentação das CARTAS 1 e 2.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,13 +21,31 @@ int main (){
     double areacarta1, areacarta2;
     
     //Variaveis do PIB das cartas 1 e 2.
-    int pibcarta1, pibcarta2;
+    double pibcarta1, pibcarta2;
     
     //Variaveis de pontos turisticos das cartas 1 e 2.
     int pontosturisticoscarta1, pontosturisticoscarta2;
     
+    //Variaveis da Densidade Populacional das cartas 1 e 2.
+    float densidade_populacional_carta1, densidade_populacional_carta2;
+    
+    //Variaveis do PIB Percapita das cartas 1 e 2.
+    float pib_percapita_carta1, pib_percapita_carta2;
+    
+    
+    
+    
+    
+    //Começo das Funcionalidades.
+    
+    
     //Pequena introdução.
     printf("Olá Bem Vindo(a) ao Super Trunfo Brasil, um jogo de carta de cidades brasileiras. \n Aqui você adicionara duas cartas ao jogo de suas cidades preferidas!! \n"); 
+    
+    
+    
+    
+    
     
     //Pedir informações da CARTA 1 ao usuario.
     
@@ -49,10 +67,25 @@ int main (){
     scanf("%lf", &areacarta1);    //Ler o que o usuario digitou.
     
     printf("Digite o PIB da Cidade da carta 1: ");   //Pedir PIB da cidade da carta 1.
-    scanf("%d", &pibcarta1);   //Ler o que o usuario digitou.
+    scanf("%lf", &pibcarta1);   //Ler o que o usuario digitou.
     
     printf("Digite a quantidade de pontos turisticos da cidade da carta 1: ");    //Pedir pontos turisticos da cidade da carta 1.
     scanf("%d", &pontosturisticoscarta1);   //Ler o que o usuario digitou.
+    
+    
+    
+    
+    
+    //Calculo densidade populacional e PIBPercapita da CARTA 1.
+    
+    //Calculo Densidade Populacional CARTA 1.
+    densidade_populacional_carta1 =(float) populacaocarta1 / areacarta1;
+    
+    //Calculo PIB Percapita CARTA 1.
+    pib_percapita_carta1 = (float) pibcarta1 / populacaocarta1;
+    
+    
+    
     
     
     //Apresentação da CARTA 1.
@@ -80,13 +113,27 @@ int main (){
     printf("Area da Cidade: %.2f \n", areacarta1);
     
     //Apresentar PIB.
-    printf("PIB da Cidade: %d \n", pibcarta1);
+    printf("PIB da Cidade: %.2f \n", pibcarta1);
     
     //Apresentar Pontos Turisticos.
     printf("Pontos Turisticos da Cidade: %d \n", pontosturisticoscarta1);
     
+    //Apresentação da Densidade Populacional.
+    printf("Densidade Populacional: %f \n", densidade_populacional_carta1);
+    
+    //Apresentação do PIB Per Capita.
+    printf("PIB Per Capita: %f \n", pib_percapita_carta1);
+    
+    
     //Diferenciar linha
     printf("========================================= \n");
+    
+    
+    
+    
+    
+    
+    
     
     //CARTA 2
     
@@ -95,6 +142,8 @@ int main (){
     
     //Diferenciar linha
     printf("========================================= \n");
+    
+    
     
     getchar();  //Tira o '\n', possibilitando usar o fgets.
     
@@ -110,16 +159,36 @@ int main (){
     fgets(cidadecarta2, 100, stdin);    //Ler o que o usuario digitou
     
     printf("Digite a população da cidade da carta 2: ");   //Pedir população da cidade da carta 2.
-    scanf("%f", &populacaocarta2);   //Ler o que o usuario digitou.
+    scanf("%d", &populacaocarta2);   //Ler o que o usuario digitou.
     
     printf("Digite a Area da Cidade da carta 2: ");  //Pedir Area da cidade da carta 2.
     scanf("%lf", &areacarta2);    //Ler o que o usuario digitou.
     
     printf("Digite o PIB da Cidade da carta 2: ");   //Pedir PIB da cidade da carta 2.
-    scanf("%f", &pibcarta2);   //Ler o que o usuario digitou.
+    scanf("%lf", &pibcarta2);   //Ler o que o usuario digitou.
     
     printf("Digite a quantidade de pontos turisticos da cidade da carta 2: ");    //Pedir pontos turisticos da cidade da carta 2.
     scanf("%d", &pontosturisticoscarta2);   //Ler o que o usuario digitou.
+    
+    
+    
+    
+    
+    
+    //Calculo densidade populacional e PIBPercapita da CARTA 2.
+    
+    //Calculo Densidade Populacional CARTA 2.
+    densidade_populacional_carta2 =(float) populacaocarta2 / areacarta2;
+    
+    //Calculo PIB Percapita CARTA 2.
+    pib_percapita_carta2 = (float) pibcarta2 / populacaocarta2;
+    
+    
+    
+    
+    
+    
+    
     
     //Apresentação da CARTA 2.
     
@@ -146,10 +215,18 @@ int main (){
     printf("Area da Cidade: %.2f \n", areacarta2);
     
     //Apresentar PIB.
-    printf("PIB da Cidade: %d \n", pibcarta2);
+    printf("PIB da Cidade: %.2f \n", pibcarta2);
     
     //Apresentar Pontos Turisticos.
     printf("Pontos Turisticos da Cidade: %d \n", pontosturisticoscarta2);
+    
+    //Apresentação da Densidade Populacional.
+    printf("Densidade Populacional: %f \n", densidade_populacional_carta2);
+    
+    //Apresentação do PIB Per Capita.
+    printf("PIB Per Capita: %f \n", pib_percapita_carta2);
+    
+    
     
     //Diferenciar linha
     printf("========================================= \n");
